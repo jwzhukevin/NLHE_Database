74d125ac2c827b923582b2068a1ae686fcd83a0c
kevin <2672497124@qq.com>
2025-03-11 09:44:12 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import click
 import os
 import sys
-from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 from markupsafe import escape
 
 WIN = sys.platform.startswith('win')
@@ -17,24 +17,17 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的
 # 在扩展类实例化前加载配置
 db = SQLAlchemy(app)
 
-@app.route('/')
-def index():
-    user = User.query.first()  # 读取用户记录
-    movies = Movie.query.all()  # 读取所有电影记录
-    return render_template('index.html', user=user, movies=movies)
-
 @app.route('/user/<name>')
 def name(name):
     return f'User:{escape(name)}'
 
 @app.cli.command()  # 注册为命令，可以传入 name 参数来自定义命令
-
 def forge():
     """Generate fake data."""
     db.create_all()
 
     # 全局的两个变量移动到这个函数内
-    name = 'Grey Li'
+    name = '1'
     movies = [
         {'title': 'My Neighbor Totoro', 'year': '1988'},
         {'title': 'Dead Poets Society', 'year': '1989'},
@@ -56,6 +49,17 @@ def forge():
 
     db.session.commit()
     click.echo('Done.')
+
+@app.cli.command()  # 注册为命令，可以传入 name 参数来自定义命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
+
+
 
 @app.route('/test')
 def test_url_for():
@@ -79,4 +83,25 @@ class Movie(db.Model):  # 表名将会是 movie
     title = db.Column(db.String(60))  # 电影标题
     year = db.Column(db.String(4))  # 电影年份
 
+@app.errorhandler(404)  # 传入要处理的错误代码
+def page_not_found(e):  # 接受异常对象作为参数
+    user = User.query.first()
+    return render_template('404.html', user=user), 404  # 返回模板和状态码
+
+
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.route('/')
+def index():
+    movies = Movie.query.all()
+    return render_template('index.html', movies=movies)
 
