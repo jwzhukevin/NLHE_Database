022168f8efc849308ec8c375bb3d05908434ba6b
jwzhukevin <2672497124@qq.com>
2025-03-25 21:40:41 +0800

    初次添加jsmol|Deploy: watchlist@master:7603abc (2025-03-25 21:40:40)

--- push.sh
+++ push.sh
@@ -12,7 +12,7 @@ REPO_NAME=$(basename "$SOURCE_REPO_DIR")
 TIMESTAMP=$(date +'%Y-%m-%d %H:%M:%S')
 
 # 提交到部署仓库
-git commit -m "修改虚拟环境名称|Deploy: ${REPO_NAME}@${BRANCH_NAME}:${COMMIT_HASH} (${TIMESTAMP})"
+git commit -m "初次添加jsmol|Deploy: ${REPO_NAME}@${BRANCH_NAME}:${COMMIT_HASH} (${TIMESTAMP})"
 
 # 推送
-git push
+git push origin master
